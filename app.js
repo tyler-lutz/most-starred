7099e311bb135cfa7938f42297a87140e27bf7bc
Tyler Lutz <66930763+tyler-lutz@users.noreply.github.com>
2023-05-20 20:18:33 -0500

    Add axios package

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const axios = require('axios');
+
 async function getMostStarredRepos()
 {
-    const response = await fetch('https://api.github.com/search/repositories?q=stars:>1&sort=stars&order=desc');
-    const data = await response.json();
-    const projects = await data.items;
+    try {
+        const reponse = await axios.get('https://api.github.com/search/repositories', {
+            params: {
+                q: 'stars:>1',
+                sort: 'stars',
+                order: 'desc'
+            }
+        });
 
-    console.log(`Most starred repos on GitHub:`);
-    projects.forEach((project, index) => {
-        console.log(`${index + 1}. ${project.name} (${project.stargazers_count} stars) - ${project.html_url} (${project.description})`)
-    });
+        const data = reponse.data;
+        const projects = data.items;
+
+        console.log('Most starred repos on GitHub:');
+        projects.forEach((project, index) => {
+            console.log(`${index + 1}. ${project.name} (${project.stargazers_count} stars) - ${project.html_url} (${project.description})`)
+        });
+    } catch (err) {
+        console.error('Error while getting most starred repos:', err.message);
+    }
 }
 
 getMostStarredRepos();
